b906eb6ef7e6c6e6a05dc6acec21a0308256fcb6
Tatiana Vaganova <athdev@yandex.ru>
2025-01-01 13:59:32 +0300

    feat: configure CORS with specific options for secure cross-origin requests

--- backend/src/app.ts
+++ backend/src/app.ts
@@ -5,26 +5,28 @@ import 'dotenv/config'
 import express, { json, urlencoded } from 'express'
 import mongoose from 'mongoose'
 import path from 'path'
-import { DB_ADDRESS } from './config'
+import { DB_ADDRESS, PORT, ORIGIN_ALLOW } from './config'
 import errorHandler from './middlewares/error-handler'
 import serveStatic from './middlewares/serverStatic'
 import routes from './routes'
 
-const { PORT = 3000 } = process.env
 const app = express()
 
 app.use(cookieParser())
 
-app.use(cors())
-// app.use(cors({ origin: ORIGIN_ALLOW, credentials: true }));
-// app.use(express.static(path.join(__dirname, 'public')));
+const corsOptions = {
+    origin: ORIGIN_ALLOW,
+    credentials: true,
+    allowedHeaders: ['Authorization', 'Content-Type'],
+}
+
+app.use(cors(corsOptions))
 
 app.use(serveStatic(path.join(__dirname, 'public')))
 
 app.use(urlencoded({ extended: true }))
 app.use(json())
 
-app.options('*', cors())
 app.use(routes)
 app.use(errors())
 app.use(errorHandler)
